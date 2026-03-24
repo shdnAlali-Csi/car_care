5c95fce4243fa35c0f51c557de5390d25c88b608
mayamahairy <m00842327@gmail.com>
2026-03-25 02:48:34 +0300

    Resolve .env merge conflict

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.car_care"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.1.12297006"
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
